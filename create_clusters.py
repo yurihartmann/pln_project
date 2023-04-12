3ab74632978623974bfb73b836089e20e18c3c7f
Yuri Hartmann <yuri.hartmann@br.experian.com>
2023-04-12 20:04:21 -0300

    12/04

--- create_clusters.py
+++ create_clusters.py
@@ -6,16 +6,17 @@ projects_dict: dict
 with open(FILE) as file:
     projects_dict = json.load(file)
 
-cluster_de_palavras_chaves = {}
+for cluster_name in ['palavras_chaves', 'depta/instituto']:
 
-for project in projects_dict:
-    for palavra_chave in project['palavras_chaves']:
+    cluster = {}
 
-        if cluster_de_palavras_chaves.get(palavra_chave):
-            cluster_de_palavras_chaves[palavra_chave] += project['titulo'] + project['resumo']
-        else:
-            cluster_de_palavras_chaves[palavra_chave] = project['titulo'] + project['resumo']
+    for project in projects_dict:
+        for palavra_chave in project[cluster_name]:
 
+            if cluster.get(palavra_chave):
+                cluster[palavra_chave] += project['titulo'] + project['resumo']
+            else:
+                cluster[palavra_chave] = project['titulo'] + project['resumo']
 
-with open(f'history_data/1/cluster_palavras_chaves.json', 'w', encoding='utf-8') as fp:
-    json.dump(cluster_de_palavras_chaves, fp, indent=4, ensure_ascii=False)
+    with open(f'history_data/1/cluster_{cluster_name.replace("/", "_")}.json', 'w', encoding='utf-8') as fp:
+        json.dump(cluster, fp, indent=4, ensure_ascii=False)
